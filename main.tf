44244a0d1f5bc1a1167fa4a61da74d721ef9a13f
Debolin <46442437+debolindhar@users.noreply.github.com>
2020-03-06 22:43:28 +0530

    Update main.tf
    Added App Service Plan & App Service script

--- main.tf
+++ main.tf
@@ -2,10 +2,32 @@ provider "azurerm" {
   version = "~> 1.33.1"
 }
 
-resource "azurerm_resource_group" "lab1" {
-  name     = "terraform-lab1"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resourcegroup"
   location = "East US"
   tags = {
     environment = "training"
+  }
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Free"
+	size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
   }
 }
