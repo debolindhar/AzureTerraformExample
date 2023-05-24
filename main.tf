b2944e462e9b28e3040d32611f75ea043b7f9ee1
Debolin <46442437+debolindhar@users.noreply.github.com>
2023-05-24 11:30:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ resource "azurerm_app_service" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
+  password = "123!@!"
 
   site_config {
     dotnet_framework_version = "v4.0"
